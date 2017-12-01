70a864f0e282d81c61a56ee30205b3407875dd6e
Harish <hthuwal@gmail.com>
2017-12-01 19:28:46 +0530

    List of safe extensions instead of target extensions

--- Python Scripts/Shrink File Names/remove_from_file_names.py
+++ Python Scripts/Shrink File Names/remove_from_file_names.py
@@ -8,14 +8,14 @@ else:
 	folder = os.getcwd()
 	to_be_removed = sys.argv[1:]
 
-extensions = [".mkv", ".m4v", ".mp4", ".srt"]
+safe_extensions = [".txt"]
 
 if len(to_be_removed) > 0:
 	for path in os.listdir(folder):
 
 		filename, ext =  os.path.splitext(path)
 		
-		if ext in extensions:
+		if ext not in safe_extensions:
 			for string in to_be_removed:
 				filename = filename.replace(string, "").strip()
 			
