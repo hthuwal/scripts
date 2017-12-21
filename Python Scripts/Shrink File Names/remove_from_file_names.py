d842285c74bdfb51e3be5d07db235344e18e55bb
Harish <hthuwal@gmail.com>
2017-12-21 20:41:19 +0530

    Action is now performed recursively in all subdirectories

--- Python Scripts/Shrink File Names/remove_from_file_names.py
+++ Python Scripts/Shrink File Names/remove_from_file_names.py
@@ -3,32 +3,39 @@
 import os
 import sys
 
-if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
-    folder = sys.argv[1]
-    to_be_removed = sys.argv[2:]
-else:
-    print "First argument should be the path to a directory"
-    sys.exit()
-
 safe_extensions = [".txt"]
 
-if len(to_be_removed) > 0:
-    for path in os.listdir(folder):
 
-        filename, ext = os.path.splitext(path)
+def clean(folder, to_be_removed):
+    if len(to_be_removed) > 0:
+        for path in os.listdir(folder):
 
-        if ext not in safe_extensions:
-            for string in to_be_removed:
-                filename = filename.replace(string, "").strip()
+            filename, ext = os.path.splitext(path)
 
-            filename = " ".join(filename.split('.')).strip()
-            newpath = filename + ext
+            if ext not in safe_extensions:
+                for string in to_be_removed:
+                    filename = filename.replace(string, "").strip()
 
-            if path != newpath:
-                print "renaming: " + path + " to " + newpath
-                os.rename(os.path.join(folder, path),
-                          os.path.join(folder, newpath))
-else:
-    print "Nothing to remove!\nPlease pass as arguments(space seperated) the strings that should be removed from the names"
+                filename = " ".join(filename.split('.')).strip()
+                newpath = filename + ext
 
-    print "Nothing to remove!\nPlease pass as arguments(space seperated) the strings that should be removed from the names"
+                sourcepath = os.path.join(folder, path)
+                targetpath = os.path.join(folder, newpath)
+
+                if path != newpath:
+                    print "renaming: " + path + " to " + newpath
+                    os.rename(sourcepath, targetpath)
+
+                if os.path.isdir(targetpath):
+                    clean(targetpath, to_be_removed)
+    else:
+        print "Nothing to remove!\nPlease pass as arguments(space seperated) the strings that should be removed from the names"
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
+        folder = sys.argv[1]
+        to_be_removed = sys.argv[2:]
+        clean(folder, to_be_removed)
+    else:
+        print "First argument should be the path to a directory"
+        sys.exit()
