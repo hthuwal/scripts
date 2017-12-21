f96a3b9d0b7e43aab70d72c099746865c331c183
Harish <hthuwal@gmail.com>
2017-12-21 20:23:28 +0530

    Pep8

--- Python Scripts/Shrink File Names/remove_from_file_names.py
+++ Python Scripts/Shrink File Names/remove_from_file_names.py
@@ -3,30 +3,32 @@
 import os
 import sys
 
-if len(sys.argv)>1 and os.path.isdir(sys.argv[1]):
-	folder = sys.argv[1]
-	to_be_removed = sys.argv[2:]
+if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
+    folder = sys.argv[1]
+    to_be_removed = sys.argv[2:]
 else:
-	print "First argument should be the path to a directory"
-	sys.exit()
+    print "First argument should be the path to a directory"
+    sys.exit()
 
 safe_extensions = [".txt"]
 
 if len(to_be_removed) > 0:
-	for path in os.listdir(folder):
+    for path in os.listdir(folder):
 
-		filename, ext =  os.path.splitext(path)
-		
-		if ext not in safe_extensions:
-			for string in to_be_removed:
-				filename = filename.replace(string, "").strip()
-			
-			filename = " ".join(filename.split('.')).strip()
+        filename, ext = os.path.splitext(path)
 
-			newpath = filename+ext
+        if ext not in safe_extensions:
+            for string in to_be_removed:
+                filename = filename.replace(string, "").strip()
 
-			if path != newpath:
-				print "renaming: " + path + " to " + newpath
-				os.rename(os.path.join(folder, path), os.path.join(folder, newpath))
+            filename = " ".join(filename.split('.')).strip()
+            newpath = filename + ext
+
+            if path != newpath:
+                print "renaming: " + path + " to " + newpath
+                os.rename(os.path.join(folder, path),
+                          os.path.join(folder, newpath))
 else:
-	print "Nothing to remove!\nPlease pass as arguments(space seperated) the strings that should be removed from the names"
+    print "Nothing to remove!\nPlease pass as arguments(space seperated) the strings that should be removed from the names"
+
+    print "Nothing to remove!\nPlease pass as arguments(space seperated) the strings that should be removed from the names"
