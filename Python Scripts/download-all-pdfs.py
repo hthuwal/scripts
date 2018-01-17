21e2f21c86a46b9fd6a49311d05bc08758c3a829
Harish <hthuwal@gmail.com>
2018-01-17 17:56:00 +0530

    Url is passed as command line argument and open an http request

--- Python Scripts/download-all-pdfs.py
+++ Python Scripts/download-all-pdfs.py
@@ -1,5 +1,6 @@
 # using beautiful soup for webpage parsing
 import sys
+import urllib2
 
 try:
     from bs4 import BeautifulSoup
@@ -7,3 +8,24 @@ except ImportError:
     print "Please download and install BeautifulSoup first"
     sys.exit(0)
 
+inputs = sys.argv
+
+if len(inputs) == 1:
+    # TODO print help message
+    pass
+else:
+    url = inputs[1].strip()
+
+    try:
+        request = urllib2.Request(url)
+        html = urllib2.urlopen(request)
+        # TODO html parsing
+
+    except KeyboardInterrupt:
+        print "\nExiting..."
+        sys.exit(1)
+
+    except Exception as e:
+        print "Something went wrong!"
+        print e
+        sys.exit(2)
