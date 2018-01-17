e46091bbaf397ac3d0a82b501f5810cc9743db0f
Harish <hthuwal@gmail.com>
2018-01-17 17:59:24 +0530

    if name == main

--- Python Scripts/download-all-pdfs.py
+++ Python Scripts/download-all-pdfs.py
@@ -8,24 +8,25 @@ except ImportError:
     print "Please download and install BeautifulSoup first"
     sys.exit(0)
 
-inputs = sys.argv
+if __name__ == '__main__':
+    inputs = sys.argv
 
-if len(inputs) == 1:
-    # TODO print help message
-    pass
-else:
-    url = inputs[1].strip()
+    if len(inputs) == 1:
+        # TODO print help message
+        pass
+    else:
+        url = inputs[1].strip()
 
-    try:
-        request = urllib2.Request(url)
-        html = urllib2.urlopen(request)
-        # TODO html parsing
+        try:
+            request = urllib2.Request(url)
+            html = urllib2.urlopen(request)
+            # TODO html parsing
 
-    except KeyboardInterrupt:
-        print "\nExiting..."
-        sys.exit(1)
+        except KeyboardInterrupt:
+            print "\nExiting..."
+            sys.exit(1)
 
-    except Exception as e:
-        print "Something went wrong!"
-        print e
-        sys.exit(2)
+        except Exception as e:
+            print "Something went wrong!"
+            print e
+            sys.exit(2)
