fa09d5faa56d51565b4729bf2e2b21b24d2062ce
Harish Thuwal <hthuwal@gmail.com>
2019-06-08 00:15:02 +0530

    Some new aliases and comments

--- shell-scripts/aliases.sh
+++ shell-scripts/aliases.sh
@@ -1,27 +1,35 @@
-# Kitty Aliases
+# -------------------------- Kitty Specific Aliases -------------------------- #
+
 if [[ $TERM == "xterm-kitty" ]]
 then
 	alias kcat="kitty +kitten icat"
 	alias kdiff="kitty +kitten diff"
 fi
 
+# --------------------------------- Clipboard -------------------------------- #
 
-# clipboard
 alias xp="xclip -out -selection clipboard"
+alias xpi="xp -t image/png"
 
-alias youtube="youtube-dl -c -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mkv"
-alias youmusic="youtube-dl -c -f bestaudio[ext=m4a] -x --audio-format mp3"
-alias subs="subliminal download -l en"
-alias bcp="rsync -ah --info=progress2"
+# ------------------------------------ MPV ----------------------------------- #
+
+alias play_selection='mpv "$(xclip -o)"'
 alias slideshow="find . * | mpv -fs --image-display-duration=1 --playlist=-"
 
-#python
-alias pie="python3.6"
-alias subl="subl3"
-alias r='ranger'
-alias o='xdg-open'
+# ----------------------------- Downloading stuff ---------------------------- #
 
-#Github
+alias youtube-dl-aria="youtube-dl --external-downloader 'aria2c' --external-downloader-args '--file-allocation=none -c -x 16 -s 16'" 
+alias youtube="youtube-dl-aria -c -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mkv"
+alias youmusic="youtube-dl-aria -c -f bestaudio[ext=m4a] -x --audio-format mp3"
+alias pd='aria2c --file-allocation=none -c -x 16 -s 16' 
+
+# ---------------------------------- python ---------------------------------- #
+
+alias pie="python3.6"
+alias ptpy='ptpython'
+
+# ------------------------------------ git ----------------------------------- #
+
 alias gst="git status"
 alias ga="git add"
 alias gc='git commit'
@@ -30,29 +38,32 @@ alias gpl='git pull origin master'
 alias gl='git log --branches --remotes --tags --graph --oneline --decorate'
 alias gcm='git checkout master'
 
-alias gapdf='wget -A pdf -m -p -E -k -K -np -nd'
+# ------------------------------- Arch related ------------------------------- #
 
-#Aria parallel download
-alias pd='aria2c --file-allocation=none -c -x 16 -s 16' 
-alias ptpy='ptpython'
-
-alias vi='vim'
-
-#aliases for arch
 alias remorphans='sudo pacman -Rns $(pacman -Qtdq)'
 alias remcache='sudo paccache -r'
+alias cleanup='yay -Sc'
+alias remove='yay -Rcns'
 
-#aliases for docker
+# ---------------------------------- Docker ---------------------------------- #
+
 alias drmi='sudo docker rmi $(sudo docker images -q)'
 alias drmc='sudo docker rm $(sudo docker ps -a -q)'
 alias dkillall='sudo docker kill $(sudo docker ps -a -q)'
 alias vpl_docker='sudo docker run --rm --privileged -p 80:80 -p 443:443 -it --user root hthuwal/vpl_docker bash -c "service vpl-jail-system start; bash"'
 
+# ------------------------------- Miscellaneous ------------------------------ #
+
+alias subl="subl3"
+alias r='ranger'
+alias o='xdg-open'
+alias subs="subliminal download -l en"
+alias bcp="rsync -ah --info=progress2"
+alias gapdf='wget -A pdf -m -p -E -k -K -np -nd'
+alias vi='vim'
 alias img2vid='ffmpeg -r 1 -f image2 -s 1920x1080 -i %03d.png -vcodec libx264 -crf 25'
-
 alias net_sucks='systemctl restart NetworkManager.service'
-
-alias iitd='ssh -D 8123 -C -q -N sri'
+alias iitd_proxy='ssh -D 8123 -C -q -N sri'
 
 xsv-head() {
     lines=${2:-100}
