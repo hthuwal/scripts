d3327571538e37d596e0dd81e0588b2ad4fae1dd
Harish Thuwal <hthuwal@gmail.com>
2025-01-06 19:43:50 +0530

    Add klogs and fix indentation

--- shell-scripts/kube.sh
+++ shell-scripts/kube.sh
@@ -32,20 +32,30 @@ alias klf='kubectl logs -f'
 
 # ----------------------------------- SCP ------------------------------------ #
 function kcp() {
-        local namespace=$1
-        local pod=$2
-        local source=$3
-        local target=$4
-        if [[ ! -d $target ]]; then
-                echo "$target is not a valid directory."
-                return 1
-        fi
-        local size=$(kubectl exec -n $namespace $pod -- du -k -d 0 "$source" | awk -F" " '{print $1}')
+    local namespace=$1
+    local pod=$2
+    local source=$3
+    local target=$4
+    if [[ ! -d $target ]]; then
+            echo "$target is not a valid directory."
+            return 1
+    fi
+    local size=$(kubectl exec -n $namespace $pod -- du -k -d 0 "$source" | awk -F" " '{print $1}')
 
-        if [[ -z ${size+x} ]] || [[ $size == "" ]]; then
-                echo "Error while estimating size of the source to be copied."
-        else
-                echo "Copying $size KB of data from $source"
-                kubectl exec -n $namespace $pod -- tar cf - "$source" | pv -s "$size"k | tar xf - -C "$target"
-        fi
+    if [[ -z ${size+x} ]] || [[ $size == "" ]]; then
+            echo "Error while estimating size of the source to be copied."
+    else
+            echo "Copying $size KB of data from $source"
+            kubectl exec -n $namespace $pod -- tar cf - "$source" | pv -s "$size"k | tar xf - -C "$target"
+    fi
+}
+
+function klogs() {
+    local service=$1
+    if [[ -z "$service" ]]; then
+        echo "Pod name is required."
+        return 1
+    fi
+    echo stern -n microservices --since 1s "^${service}" -c "${service}"
+    stern -n microservices --since 1s "^${service}" -c "${service}"
 }
